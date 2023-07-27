d83e83d1370e64275b94eb028a01c94a9056c509
mhk98 <mohsinkabirseo@gmail.com>
2023-07-27 11:44:10 +0600

    deploy v2 update

--- src/components/InnerPages/Intelligent/IntelligentTech4.jsx
+++ src/components/InnerPages/Intelligent/IntelligentTech4.jsx
@@ -1,7 +1,197 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import product1 from "../../../../images/Tripod Turnstile.png";
+import product2 from "../../../../images/high speed gate.jpg";
+import product3 from "../../../../images/Flap Barrier gate.jpg";
+import product4 from "../../../../images/Swing barrier gate.png";
+import product5 from "../../../../images/full height turnstile.jpg";
+import product6 from "../../../../images/Barrier gate.jpg";
+import product7 from "../../../../images/Car Parking System.jpg";
 
 export default function IntelligentTech4() {
   return (
-    <div>IntelligentTech4</div>
-  )
+    <div>
+      <div className="cart_parking">
+        <div>
+          <h5>Tripod Turnstile</h5>
+          <p>
+            Our web application development department is equipped with the
+            state-of-the-art equipment and a highly-trained team of development
+            that generates outstanding ideas for our client’s project. They work
+            closely with you to generate optimal outcomes for your application/
+            software concerns. We complete your project within the given
+            deadline.
+          </p>
+          <div className="purchase" style={{ marginTop: "10px" }}>
+            <a className="butn butn-md butn-bord radius-30">
+              <span>LEARN MORE </span>
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            style={{ width: "370px", height: "500px" }}
+            src={product1}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="cart_parking">
+        <div>
+          <Image
+            style={{ width: "500px", height: "400px" }}
+            src={product2}
+            alt=""
+          />
+        </div>
+
+        <div>
+          <h5>Flap Barrier gate</h5>
+          <p>
+            We specialize in design and development for mobile apps and
+            websites. We design websites and mobile apps that are further
+            facilitated with a high-performance rate. Our team of developers and
+            designers at Createabit Design are acquainted with the creation and
+            development of iOS and Android apps that are user-specific and
+            mobile-friendly-specific.
+          </p>
+          <div className="purchase" style={{ marginTop: "10px" }}>
+            <a className="butn butn-md butn-bord radius-30">
+              <span>LEARN MORE </span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="cart_parking">
+        <div>
+          <h5>Flap Barrier gate</h5>
+          <p>
+            Our web application development department is equipped with the
+            state-of-the-art equipment and a highly-trained team of development
+            that generates outstanding ideas for our client’s project. They work
+            closely with you to generate optimal outcomes for your application/
+            software concerns. We complete your project within the given
+            deadline.
+          </p>
+          <div className="purchase" style={{ marginTop: "10px" }}>
+            <a className="butn butn-md butn-bord radius-30">
+              <span>LEARN MORE </span>
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            style={{ width: "500px", height: "400px" }}
+            src={product3}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="cart_parking">
+        <div>
+          <Image
+            style={{ width: "500px", height: "400px" }}
+            src={product4}
+            alt=""
+          />
+        </div>
+
+        <div>
+          <h5>Swing barrier gate</h5>
+          <p>
+            We specialize in design and development for mobile apps and
+            websites. We design websites and mobile apps that are further
+            facilitated with a high-performance rate. Our team of developers and
+            designers at Createabit Design are acquainted with the creation and
+            development of iOS and Android apps that are user-specific and
+            mobile-friendly-specific.
+          </p>
+          <div className="purchase" style={{ marginTop: "10px" }}>
+            <a className="butn butn-md butn-bord radius-30">
+              <span>LEARN MORE </span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="cart_parking">
+        <div>
+          <h5>Full Height Turnstile</h5>
+          <p>
+            Our web application development department is equipped with the
+            state-of-the-art equipment and a highly-trained team of development
+            that generates outstanding ideas for our client’s project. They work
+            closely with you to generate optimal outcomes for your application/
+            software concerns. We complete your project within the given
+            deadline.
+          </p>
+          <div className="purchase" style={{ marginTop: "10px" }}>
+            <a className="butn butn-md butn-bord radius-30">
+              <span>LEARN MORE </span>
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            style={{ width: "500px", height: "400px" }}
+            src={product5}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="cart_parking">
+        <div>
+          <Image
+            style={{ width: "500px", height: "400px" }}
+            src={product6}
+            alt=""
+          />
+        </div>
+
+        <div>
+          <h5>Barrier gate</h5>
+          <p>
+            We specialize in design and development for mobile apps and
+            websites. We design websites and mobile apps that are further
+            facilitated with a high-performance rate. Our team of developers and
+            designers at Createabit Design are acquainted with the creation and
+            development of iOS and Android apps that are user-specific and
+            mobile-friendly-specific.
+          </p>
+          <div className="purchase" style={{ marginTop: "10px" }}>
+            <a className="butn butn-md butn-bord radius-30">
+              <span>LEARN MORE </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="cart_parking">
+        <div>
+          <h5>Car Parking System</h5>
+          <p>
+            Our web application development department is equipped with the
+            state-of-the-art equipment and a highly-trained team of development
+            that generates outstanding ideas for our client’s project. They work
+            closely with you to generate optimal outcomes for your application/
+            software concerns. We complete your project within the given
+            deadline.
+          </p>
+          <div className="purchase" style={{ marginTop: "10px" }}>
+            <a className="butn butn-md butn-bord radius-30">
+              <span>LEARN MORE </span>
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            style={{ width: "500px", height: "400px" }}
+            src={product7}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
