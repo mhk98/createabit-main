6ec2e4821cbaa06a0d24530722558165a4e96e7d
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2023-11-07 11:24:49 +0600

    latest update

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
-// const nextConfig = {
-//   reactStrictMode: false,
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "css")],
-//   },
-//   trailingSlash: true,
-//   devIndicators: {
-//     buildActivity: false,
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: false,
-//   },
-// };
-
 module.exports = {
   images: {
     domains: ["localhost", "createabit-backend.onrender.com"],
