78c1a8011e2b92e263c0bb53f0cf8950a7bbf3c4
mhk98 <mohsinkabirseo@gmail.com>
2023-11-18 11:48:03 +0600

    ready for deploy

--- src/components/PaymentInfo/Payment.jsx
+++ src/components/PaymentInfo/Payment.jsx
@@ -4,7 +4,9 @@ import CheckoutForm from "./CheckoutForm";
 import { loadStripe } from "@stripe/stripe-js";
 
 const Payment = () => {
-  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+  const stripePromise = loadStripe(
+    "pk_test_51L1rVDH0VF27tpW3LEs6Z66fVvzRIoh08xSbY4zSyiG03Q2pwNVzhq8JoYv4s4xF4zwObI3gLNUjus6dFj0ltrvr00m17agrob"
+  );
   return (
     <div>
       <Elements stripe={stripePromise}>
