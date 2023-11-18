4b22d91d877a633ddaa9c7a32cfe66e5308bbe57
mhk98 <mohsinkabirseo@gmail.com>
2023-11-18 15:24:07 +0600

    update

--- src/components/PaymentInfo/PaymentResult.jsx
+++ src/components/PaymentInfo/PaymentResult.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const PaymentResult = () => {
   const Result = localStorage.getItem("payment-status");
   return (
--- src/pages/dark/page-purchaseStatus/index.jsx
+++ src/pages/dark/page-purchaseStatus/index.jsx
@@ -1,44 +1,44 @@
-import Loader from "@/components/Common/Loader";
-import Head from "next/head";
-import { useEffect } from "react";
-import DarkNavbar from "../dark-navbar";
-import PaymentResult from "@/components/PaymentInfo/PaymentResult";
-import DarkFooter from "../dark-footer";
-import DefaultLayout from "@/layouts/default";
+// import Loader from "@/components/Common/Loader";
+// import Head from "next/head";
+// import { useEffect } from "react";
+// import DarkNavbar from "../dark-navbar";
+// import PaymentResult from "@/components/PaymentInfo/PaymentResult";
+// import DarkFooter from "../dark-footer";
+// import DefaultLayout from "@/layouts/default";
 
-function PurchaseStatus() {
-  useEffect(() => {
-    document.body.classList.add("main-bg");
-    // Additional setup if needed
+// function PurchaseStatus() {
+//   useEffect(() => {
+//     document.body.classList.add("main-bg");
+//     // Additional setup if needed
 
-    return () => document.body.classList.remove("main-bg");
-  }, []);
+//     return () => document.body.classList.remove("main-bg");
+//   }, []);
 
-  const metadata = {
-    subTitle: "PURCHASE",
-    title: "Details.",
-  };
+//   const metadata = {
+//     subTitle: "PURCHASE",
+//     title: "Details.",
+//   };
 
-  return (
-    <>
-      <Head>
-        <title>Createabit - Purchase Details</title>
-      </Head>
+//   return (
+//     <>
+//       <Head>
+//         <title>Createabit - Purchase Details</title>
+//       </Head>
 
-      <Loader />
-      <div id="smooth-wrapper">
-        <DarkNavbar />
-        <div id="smooth-content">
-          <main className="main-bg">
-            <PaymentResult />
-          </main>
-          <DarkFooter />
-        </div>
-      </div>
-    </>
-  );
-}
+//       <Loader />
+//       <div id="smooth-wrapper">
+//         <DarkNavbar />
+//         <div id="smooth-content">
+//           <main className="main-bg">
+//             <PaymentResult />
+//           </main>
+//           <DarkFooter />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 
-PurchaseStatus.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>;
+// PurchaseStatus.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>;
 
-export default PurchaseStatus;
+// export default PurchaseStatus;
