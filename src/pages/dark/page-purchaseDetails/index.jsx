1d58706604f9fa92a2e7da81c6656364468a9fcd
mhk98 <mohsinkabirseo@gmail.com>
2023-11-18 15:53:27 +0600

    update

--- src/pages/dark/page-purchaseDetails/index.jsx
+++ src/pages/dark/page-purchaseDetails/index.jsx
@@ -1,43 +1,43 @@
-// import Loader from "@/components/Common/Loader";
-// import Head from "next/head";
-// import DarkNavbar from "../dark-navbar";
-// import CheckoutForm from "@/components/PaymentInfo/CheckoutForm";
-// import DarkFooter from "../dark-footer";
-// import DefaultLayout from "@/layouts/default";
+import Loader from "@/components/Common/Loader";
+import Head from "next/head";
+import DarkNavbar from "../dark-navbar";
+import CheckoutForm from "@/components/PaymentInfo/CheckoutForm";
+import DarkFooter from "../dark-footer";
+import DefaultLayout from "@/layouts/default";
 
-// function PurchaseDetails() {
-//   useEffect(() => {
-//     document.body.classList.add("main-bg");
-//     // Additional setup if needed
+function PurchaseDetails() {
+  useEffect(() => {
+    document.body.classList.add("main-bg");
+    // Additional setup if needed
 
-//     return () => document.body.classList.remove("main-bg");
-//   }, []);
+    return () => document.body.classList.remove("main-bg");
+  }, []);
 
-//   const metadata = {
-//     subTitle: "PURCHASE",
-//     title: "Details.",
-//   };
+  const metadata = {
+    subTitle: "PURCHASE",
+    title: "Details.",
+  };
 
-//   return (
-//     <>
-//       <Head>
-//         <title>Createabit - Purchase Details</title>
-//       </Head>
+  return (
+    <>
+      <Head>
+        <title>Createabit - Purchase Details</title>
+      </Head>
 
-//       <Loader />
-//       <div id="smooth-wrapper">
-//         <DarkNavbar />
-//         <div id="smooth-content">
-//           <main className="main-bg">
-//             <CheckoutForm />
-//           </main>
-//           <DarkFooter />
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+      <Loader />
+      <div id="smooth-wrapper">
+        <DarkNavbar />
+        <div id="smooth-content">
+          <main className="main-bg">
+            <CheckoutForm />
+          </main>
+          <DarkFooter />
+        </div>
+      </div>
+    </>
+  );
+}
 
-// PurchaseDetails.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>;
+PurchaseDetails.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>;
 
-// export default PurchaseDetails;
+export default PurchaseDetails;
